1dae86f96b830b7bf8a473777129a39004dbe6f3
muj-programmer <nick.muj.programmer@gmail.com>
2018-09-23 17:18:12 +0530

    working state achieved

--- app/src/main/java/com/example/android/ticktock/MainActivity.java
+++ app/src/main/java/com/example/android/ticktock/MainActivity.java
@@ -2,10 +2,8 @@ package com.example.android.ticktock;
 
 import android.content.Context;
 import android.graphics.Color;
-import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ScrollView;
